66fe8175c45cc317bd5bffe8d63e83763db255e0
pavanyadavk <pavanyadav.k@outlook.com>
2025-07-04 10:57:39 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+  # Example for Amazon Linux 2 in us-east-1
+  default     = "ami-0c55b159cbfafe1f0"
+}
